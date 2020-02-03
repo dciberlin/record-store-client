fff40122554b3e86bc3cc89999c63c26e4f1c6a0
Vasileios Psychas <vaspsych@gmail.com>
2020-02-03 09:49:56 +0100

    Clean up and set up
    Installed node-sass

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Record store</p>
+    </div>
+  );
+}
+
+export default App;
